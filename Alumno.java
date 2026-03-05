a7d64ca267aff5047e5d9d9f27031b5acb195e51
AxelLJ57 <20253rd038@utez.edu.mx>
2026-03-05 15:32:27 -0600

    Se declararon los setters

--- Alumno.java
+++ Alumno.java
@@ -4,4 +4,21 @@ public class Alumno{
     private double calificacion1=0;
     private double calificacion2=0;
     private double calificacion3;
+
+    //Setters
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public void setMatricula(String matricula){
+        this.matricula=matricula;
+    }
+    public void setCalificacion1(double calificacion1){
+        this.calificacion1=calificacion1;
+    }
+    public void setCalificacion2(double calificacion2){
+        this.calificacion2=calificacion2;
+    }
+    public void setCalificacion3(double calificacion3){
+        this.calificacion3=calificacion3;
+    }
 }
