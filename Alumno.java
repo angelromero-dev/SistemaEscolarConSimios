b482b05df72fab1c45d383bb82d101ca8899d3d2
AxelLJ57 <20253rd038@utez.edu.mx>
2026-03-05 16:02:54 -0600

    Se corrigieron errores y se eliminaron los sets y getters

--- Alumno.java
+++ Alumno.java
@@ -3,61 +3,34 @@ public class Alumno{
     private String matricula;
     private double calificacion1=0;
     private double calificacion2=0;
-    private double calificacion3;
+    private double calificacion3=0;
+    private double promedio=0;
 
-    //Setters
-    public void setNombre(String nombre){
-        this.nombre=nombre;
-    }
-
-    public void setMatricula(String matricula){
-        this.matricula=matricula;
-    }
-
-    public void setCalificacion1(double calificacion1){
-        this.calificacion1=calificacion1;
-    }
-
-    public void setCalificacion2(double calificacion2){
-        this.calificacion2=calificacion2;
-    }
-
-    public void setCalificacion3(double calificacion3){
-        this.calificacion3=calificacion3;
-    }
-
-    //Getters
-    public String getNombre(){
-        return this.nombre;
-    }
-
-    public String getMatricula(){
-        return this.matricula;
-    }
-
-    public double getCalificacion1(){
-        return this.calificacion1;
-    }
-
-     public double getCalificacion2(){
-        return this.calificacion2;
-    }
     
-     public double getCalificacion3(){
-        return this.calificacion3;
-    }
-
     //Constructor vacio
     public Alumno(){
     }
 
     //Constructor con todo
     public Alumno(String nombre, String matricula, 
-        double calificacion1, double calificacion2, double calificacion3){
+        double calificacion1, double calificacion2, double calificacion3, double promedio){
             this.nombre=nombre;
             this.matricula=matricula;
             this.calificacion1=calificacion1;
             this.calificacion2=calificacion2;
             this.calificacion3=calificacion3;
+            this.promedio=promedio;
+    }
+
+    //Calcular promedio
+    public void calcularPromedio(){
+        promedio=calificacion1+calificacion2+calificacion3;
+    }
+
+    //Mostrar alumno
+    public void mostrarAlumno(){
+        System.out.println("Nombre: "+getNombre());
+        System.out.println("Matricula: ");
+        System.out.println("Promedio");
     }
 }
