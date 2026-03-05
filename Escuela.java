6bd0bf8592181c30ccbe42fd99949ae51c4e258a
David-Flores1998 <20253rd041@utez.edu.mx>
2026-03-05 16:18:30 -0600

    se termino x2

--- Escuela.java
+++ Escuela.java
@@ -1,14 +1,16 @@
 public class Escuela extends Alumno {
-    private String Escuela;
-    private String Alumno;
+    private String alumno;
 
     public Escuela(String nombre, String matricula, 
-        double calificacion1, double calificacion2, double calificacion3){
-            super()
+        double calificacion1, double calificacion2, double calificacion3, String alumno){
 
+            super(nombre, matricula,calificacion1,calificacion2,calificacion3 );
+
+            this.alumno=alumno;        
         }
-
-
-
-    
+        
+    public void mostrarAlumno(){
+       super.mostrarAlumno();
+    }
+        
 }
