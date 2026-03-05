fb0204a7a5e96e7f5a77bb5a95a7ce1d73333066
David-Flores1998 <20253rd041@utez.edu.mx>
2026-03-05 15:55:15 -0600

    nombramos variables

--- Escuela.java
+++ Escuela.java
@@ -1,3 +1,14 @@
 public class Escuela extends Alumno {
+    private String Escuela;
+    private String Alumno;
+
+    public Escuela(String nombre, String matricula, 
+        double calificacion1, double calificacion2, double calificacion3){
+            super()
+
+        }
+
+
+
     
 }
