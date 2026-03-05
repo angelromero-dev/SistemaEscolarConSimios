806c95855f95256de3a45cec51e8bd6e37996bf9
AxelLJ57 <20253rd038@utez.edu.mx>
2026-03-05 15:28:43 -0600

    Se declararon los atributos como privados

---
+++ Alumno.java
@@ -0,0 +1,7 @@
+public class Alumno{
+    private String nombre;
+    private String matricula;
+    private double calificacion1=0;
+    private double calificacion2=0;
+    private double calificacion3;
+}
